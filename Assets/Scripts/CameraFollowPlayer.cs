d9103c30090154ff38a14d2600f1c72cc4b469dd
ella <ellarosenlof@hotmail.com>
2021-09-12 22:33:51 +0200

    bazinga

---
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    public Transform playerShip;
+    void LateUpdate()
+    {
+        transform.position = playerShip.position;
+    }
+
+}
---
+++ Assets/Scripts/shooting.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour
+{
+    public Transform shootingBullet;
+    public GameObject playerBullet;
+
+    public float bulletForce = 20f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) 
+        {
+            Shoot();
+
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(playerBullet, shootingBullet.position, shootingBullet.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(shootingBullet.up * bulletForce, ForceMode2D.Impulse);
+    }
+}
