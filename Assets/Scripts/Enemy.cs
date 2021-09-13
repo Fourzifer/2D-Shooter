3bcd56329d790c5ca23bb0100bbb11da9498bd10
Liam Forsberg <fourzifer@gmail.com>
2021-09-13 13:16:37 +0200

    Enemies follow the player

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,10 +8,16 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     private int enemyHealth;
 
+    public Transform player;
+    private float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
     // Start is called before the first frame update
     void Start()
     {
         //enemyHealth = 2;
+        rb = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -22,8 +28,24 @@ public class Enemy : MonoBehaviour
             Destroy(gameObject);
             enemiesLeft--;
         }
+
+        Vector3 direction = player.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+        direction.Normalize();
+        movement = direction;
     }
-    
+
+    private void FixedUpdate()
+    {
+        moveCharacter(movement);
+    }
+
+    void moveCharacter(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "PlayerBullet")
