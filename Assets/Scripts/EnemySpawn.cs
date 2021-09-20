e166ba8b1c465d1624a430116bb135a5ad29d41c
Liam Forsberg <fourzifer@gmail.com>
2021-09-20 11:52:08 +0200

    Adaptable enemy spawn

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -10,6 +10,8 @@ public class EnemySpawn : MonoBehaviour
     private float enemyYPos;
     private float enemySpawnTimer;
 
+    private int currentScene;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,8 +24,35 @@ public class EnemySpawn : MonoBehaviour
         bool enemySpawned = false;
         while (!enemySpawned)
         {
-            enemyXPos = Random.Range(-25, 25);
-            enemyYPos = Random.Range(-25, 25);
+            switch (currentScene)
+            {
+                case 0:
+                    print("Level 1 Spawn");
+                    enemyXPos = Random.Range(-25, 25);
+                    enemyYPos = Random.Range(-25, 25);
+                    break;
+                case 1:
+                    print("Level 2 Spawn");
+                    enemyXPos = Random.Range(-25, 25);
+                    enemyYPos = Random.Range(-25, 25);
+                    break;
+                case 2:
+                    print("Level 3");
+                    break;
+                case 3:
+                    print("Level 4");
+                    break;
+                case 4:
+                    print("Level 5");
+                    break;
+                default:
+                    enemyXPos = Random.Range(-25, 25);
+                    enemyYPos = Random.Range(-25, 25);
+                    print("default spawn");
+                    break;
+            }
+
+
 
             Vector3 enemyPosition = new Vector3(enemyXPos, enemyYPos, 0f);
 
