d15085c2c79ff2cf6859979d4c8efbec9489ede9
Ella <ellarosenlof@hotmail.com>
2021-09-28 15:33:47 +0200

    WIP intro

---
+++ Assets/Scripts/ImageSwitch.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageSwitch : MonoBehaviour
+{
+    public Image[] tutorialImage;
+    public int imageAmount;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
